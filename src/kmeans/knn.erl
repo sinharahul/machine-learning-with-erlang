b8422f55da5b5b83e3c0d482dec303817bfadb6f
sinharahul <rahulsinha@RAHULs-Mac-mini-3.local>
2016-07-16 18:43:53 -0400

    first commit

---
+++ src/kmeans/knn.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author rahulsinha
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%% Implement  a K means classifier .
+%%% @end
+%%% Created : 16. Jul 2016 6:40 PM
+%%%-------------------------------------------------------------------
+-module(knn).
+-author("rahulsinha").
+
+%% API
+-export([]).
